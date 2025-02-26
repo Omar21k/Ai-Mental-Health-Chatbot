1583bfb225beba077a95423c1ec5585abaa13196
samir-simpson <samirsimpson2007@gmail.com>
2025-02-26 18:26:27 -0500

    should work

--- database.py
+++ database.py
@@ -10,7 +10,8 @@ def create_database():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id TEXT NOT NULL,
             date TEXT NOT NULL,
-            messages TEXT NOT NULL
+            user_messages TEXT NOT NULL, 
+            bot_messages TEXT NOT NULL
         )
     '''
     cur.execute(table) #If the table doesn't exist this makes one  
@@ -18,6 +19,24 @@ def create_database():
     con.close() 
 
 #This will work to insert user conversations
-def logger(): 
+def logger(user_id, user_messages, bot_messages): 
     con = sq.connect("conversations.db") 
     cur = con.cursor() 
+
+    date = dt.now().strftime("%Y-%m-%d %H:%M:%S") #sets the date of the message to when it was sent
+    cursor.execute("INSERT INTO conversations (user_id, date, user_messages, bot_messages) VALUES (?,?,?,?)", 
+                   (user_id, date, user_messages, bot_messages)) 
+    #line 26-28 sets a group of messages (question + bot response) into the database with their time 
+    con.commit()
+    con.close()  
+
+#This will retrieve a set of messages 
+def grabber(user_id): 
+    con = sq.connect("conversations.db")
+    cur = con.cursor()
+    
+    cur.execute("SELECT date, conversation FROM conversations WHERE user_id = ? ORDER BY date DESC", (user_id))
+    chats = cur.fetchall()
+    
+    con.close()
+    return chats
